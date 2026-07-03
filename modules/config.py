501448b304a0c8cda496940f870df7aa3fda783b
salvioneto33 <salvioneto33@gmail.com>
2026-07-03 11:42:59 -0300

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,1 +1,24 @@
+import json
 
+
+class Config:
+
+    def __init__(self):
+        with open("config.json", "r", encoding="utf-8") as file:
+            self.data = json.load(file)
+
+    @property
+    def max_games(self):
+        return self.data["max_games"]
+
+    @property
+    def min_discount(self):
+        return self.data["min_discount"]
+
+    @property
+    def max_price(self):
+        return self.data["max_price"]
+
+    @property
+    def min_reviews(self):
+        return self.data["min_reviews"]
