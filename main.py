2c9b955ac28cd35b5bce14c15f6ee6de865a4345
salvioneto33 <salvioneto33@gmail.com>
2026-07-03 11:25:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 from modules.steam_service import SteamService
 from modules.steam_api import SteamAPI
-from modules.discord_webhook import DiscordWebhook
 
 service = SteamService()
 steam = SteamAPI()
-discord = DiscordWebhook()
 
 deal = service.get_deals(limit=1)[0]
 
 appid = deal["steamAppID"]
 
-app = steam.get_app(appid)
+prices = steam.get_prices([appid])
 
-if not app.get("success"):
-    raise Exception("Erro ao consultar Steam")
-
-data = app["data"]
-
-game = {
-    "name": data["name"],
-    "image": data["header_image"],
-    "price": deal["salePrice"],
-    "discount": round(float(deal["savings"])),
-    "url": f"https://store.steampowered.com/app/{appid}/"
-}
-
-discord.send_game(game)
-
-print("Promoção enviada para o Discord com sucesso!")
+print(prices)
