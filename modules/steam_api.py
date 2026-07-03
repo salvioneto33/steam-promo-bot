5571a5c0f3e30bda98a51e6625deb2b6bcdb5b6f
salvioneto33 <salvioneto33@gmail.com>
2026-07-03 11:24:11 -0300

    Update steam_api.py

--- modules/steam_api.py
+++ modules/steam_api.py
@@ -4,19 +4,21 @@ import requests
 class SteamAPI:
     URL = "https://store.steampowered.com/api/appdetails"
 
-    def get_app(self, appid):
+    def get_prices(self, appids):
+        if isinstance(appids, list):
+            appids = ",".join(str(i) for i in appids)
+
         response = requests.get(
             self.URL,
             params={
-                "appids": appid,
+                "appids": appids,
                 "cc": "br",
-                "l": "brazilian"
+                "l": "brazilian",
+                "filters": "price_overview"
             },
-            timeout=15
+            timeout=20
         )
 
         response.raise_for_status()
 
-        data = response.json()
-
-        return data.get(str(appid), {})
+        return response.json()
