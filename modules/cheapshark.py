361449fce236366ba9e1cd752cd07f155c815bcd
salvioneto33 <salvioneto33@gmail.com>
2026-07-03 11:11:19 -0300

    Create cheapshark.py

---
+++ modules/cheapshark.py
@@ -0,0 +1,17 @@
+import requests
+
+
+class CheapShark:
+    URL = "https://www.cheapshark.com/api/1.0/deals"
+
+    def get_deals(self, limit=20):
+        params = {
+            "storeID": 1,      # Steam
+            "pageSize": limit,
+            "sortBy": "Savings"
+        }
+
+        response = requests.get(self.URL, params=params, timeout=15)
+        response.raise_for_status()
+
+        return response.json()
