83f426c2bb094c92f8709da2c583f637ffda6253
salvioneto33 <salvioneto33@gmail.com>
2026-07-03 11:48:06 -0300

    Update scorer.py

--- modules/scorer.py
+++ modules/scorer.py
@@ -1,1 +1,20 @@
+import math
 
+
+class Scorer:
+
+    def calculate(self, discount, price, reviews):
+
+        score = 0
+
+        # Desconto
+        score += discount
+
+        # Reviews
+        if reviews > 0:
+            score += math.log10(reviews) * 10
+
+        # Preço (quanto menor, melhor)
+        score += max(0, 100 - price)
+
+        return round(score, 2)
