46f28a56a7bdf0e217b07b5ef936f62b582695e9
salvioneto33 <salvioneto33@gmail.com>
2026-07-03 11:17:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ deal = service.get_deals(limit=1)[0]
 
 appid = deal["steamAppID"]
 
-print(f"Buscando AppID {appid}...")
-
 app = steam.get_app(appid)
 
-print(app)
+if not app.get("success"):
+    raise Exception("Erro ao consultar Steam")
+
+data = app["data"]
+
+print(f"Nome: {data['name']}")
+print(f"AppID: {appid}")
+print(f"Imagem: {data['header_image']}")
+print(f"Preço: ${deal['salePrice']}")
+print(f"Desconto: {round(float(deal['savings']))}%")
+print(f"Link: https://store.steampowered.com/app/{appid}/")
