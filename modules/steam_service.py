7481493a050fd546dbbc9d1f8214a12bd712c6b9
salvioneto33 <salvioneto33@gmail.com>
2026-07-03 11:12:06 -0300

    Update steam_service.py

--- modules/steam_service.py
+++ modules/steam_service.py
@@ -1,17 +1,10 @@
-import requests
+from modules.cheapshark import CheapShark
 
 
 class SteamService:
-    URL = "https://www.cheapshark.com/api/1.0/deals"
+
+    def __init__(self):
+        self.cheapshark = CheapShark()
 
     def get_deals(self, limit=20):
-        params = {
-            "storeID": 1,      # Steam
-            "pageSize": limit,
-            "sortBy": "Savings"
-        }
-
-        response = requests.get(self.URL, params=params, timeout=15)
-        response.raise_for_status()
-
-        return response.json()
+        return self.cheapshark.get_deals(limit)
