dd76f69bbc0f7b9af2c70b4162b0e79be6e9cbf3
salvioneto33 <salvioneto33@gmail.com>
2026-07-03 11:19:49 -0300

    Update discord_webhook.py

--- modules/discord_webhook.py
+++ modules/discord_webhook.py
@@ -1,1 +1,27 @@
+import os
+import requests
 
+
+class DiscordWebhook:
+
+    def __init__(self):
+        self.webhook = os.environ["DISCORD_WEBHOOK"]
+
+    def send_game(self, game):
+
+        payload = {
+            "embeds": [
+                {
+                    "title": game["name"],
+                    "url": game["url"],
+                    "description": f"🔥 **{game['discount']}% OFF**\n💰 **US$ {game['price']}**",
+                    "image": {
+                        "url": game["image"]
+                    },
+                    "color": 3066993
+                }
+            ]
+        }
+
+        response = requests.post(self.webhook, json=payload, timeout=15)
+        response.raise_for_status()
