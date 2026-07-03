0816d6908ea5fbf67839df4508579c5274dbbc8c
salvioneto33 <salvioneto33@gmail.com>
2026-07-03 10:53:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print("===================================")
-print("Steam Promo BR iniciado com sucesso")
-print("===================================")
+from modules.steam_api import buscar_promocoes
+
+print("Buscando promoções...\n")
+
+jogos = buscar_promocoes()
+
+for jogo in jogos:
+    print(jogo["nome"])
+    print(f'Desconto: {jogo["desconto"]}%')
+    print(f'Preço: R$ {jogo["preco_final"]:.2f}')
+    print("-" * 40)
